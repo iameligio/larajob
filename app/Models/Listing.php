7b4348ba7500c31d4ba57f14decb6decfd31ceb3
Eligio Tapiador Jr <etapiador@gmail.com>
2023-07-18 18:25:37 +0800

    create listing and listing_user pivot tabe

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Listing extends Model
 {
@@ -21,4 +22,11 @@ class Listing extends Model
         'featured_image',
         'slug'
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)
+        ->withPivot('shortlisted')
+        ->withTimestamps();
+    }
 }
